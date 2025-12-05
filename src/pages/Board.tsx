9b5442e7301f1113c8a77da9f3ffa8eba96880ff
kimbj1985-prog <kimbj1985@gmail.com>
2025-12-05 21:42:33 +0900

    제품 배열 map처리

--- src/pages/Board.tsx
+++ src/pages/Board.tsx
@@ -2,9 +2,28 @@ import Title from "../ui/Title"
 
 
 export default function Board() {
+  
+  const boardlist: string[] = [
+    "첫 번째 게시글입니다.",
+    "새로운 공지사항을 알려드리니다.",
+    "행사 일정 변경 안내드립니다.",
+    "고객 문의 답변 관련 안내",
+    "서비스 점검 안내드립니다.",
+    "커뮤니티 이용 가이드라인",
+    "자주 묻는 질문 모음",
+    "신규 기능 업데이트 안내",
+    "오늘의 소소한 이야기 공유",
+    "커뮤니티 참여 이벤트 안내",
+  ];
+
   return (
     <section>
       <Title bigtitle="커뮤니티" subtitle="자유롭게 의견을 나누는 공간입니다."></Title>
+      <ul className="text-white text-[36px] mx-auto w-[1024px] flex flex-col gap-4 mt-20">
+      {
+       boardlist.map((item) => <li className="border-b-2">{item}</li>)
+      }
+      </ul>
     </section>
   )
 }
--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -2,9 +2,49 @@ import Title from "../ui/Title"
 
 
 export default function Product() {
+  type Product = {
+  name: string;
+  price: number;
+  img : string;
+};
+
+const products: Product[] = [
+  { name: "프리미엄 커피 머신", price: 129000, img : "2.jpg" },
+  { name: "블루투스 이어폰", price: 59000, img : "3.jpg" },
+  { name: "게이밍 키보드", price: 89000, img : "4.jpg" },
+  { name: "USB-C 고속 충전기", price: 19000, img : "5.jpg" },
+  { name: "무선 마우스", price: 24000, img : "5.jpg" },
+  { name: "휴대용 보조배터리", price: 33000, img : "4.jpg" },
+  { name: "데스크 LED 스탠드", price: 42000, img : "3.jpg" },
+  { name: "스마트 체중계", price: 29000, img : "2.jpg" },
+  { name: "탁상용 공기청정기", price: 76000, img : "5.jpg" },
+  { name: "방수 블루투스 스피커", price: 52000, img : "3.jpg" }
+];
   return (
     <section>
       <Title bigtitle="제품소개" subtitle="25년 업계 최초 인증제품을 소개합니다."></Title>
+       <ul className="grid grid-cols-3 gap-8 w-[1024px] mx-auto mt-40">
+      {
+       //products.map(() => ())
+       //products.map(() => { return()})
+
+       products.map((v) => (
+                           <li className="bg-white rounded-[20px] p-4">
+                              <div>
+                                <img src={`/${v.img}`} ></img>
+                              </div>
+                              <div className="flex justify-between py-4">
+                                   <p >{v.name}</p>
+                                   <p className="price text-[#ff5722]">
+                                    {v.price}
+                                    <span className="text-[#555]">원</span>
+                                   </p>
+                              </div>
+                          </li>
+          )
+      )
+      }
+      </ul>
     </section>
   )
 }
