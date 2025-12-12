074d8ac83ad807fc73d42d1610ee44da56bf4631
kimbj1985-prog <kimbj1985@gmail.com>
2025-12-12 21:47:12 +0900

    테이윈드 디자인시스템활용,스와이퍼 스타일 모듈,json외 타입선언

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,36 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors : {
+       
+        main : "#ac441e",
+        gray : "#707070",
+        blck : "#0A0909"
+      },
+      fontFamily :{
+        kr : ['Noto Sans KR', 'sans-serif'],
+        en : ['Montserrat', 'sans-serif'],
+      },
+      fontSize : {
+        title : '28px',
+        subtitle : '18px',
+        base : '16px',
+        sm : '14px',
+        xs : '12px',
+      },
+      fontWeight : {
+        extraLight : '100',
+        light : '100',
+        thin : '300',
+        regular : '400',
+        medium : '500',
+        semiBold : '600',
+        bold : '700',
+        black : '900',     
+      }
   },
   plugins: [],
+}
 }
 
